7b01912fe2fc6cfb49b916af390f94e6ecd7a9ae
Lucas de Aquino <88683508+luucasaquino@users.noreply.github.com>
2022-04-07 16:12:46 -0300

    App 6

---
+++ app06_aleatorio/home.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+ 
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+ 
+  @override
+  _HomeState createState() => _HomeState();
+}
+ 
+class _HomeState extends State<Home> {
+  TextEditingController aleatorioController = TextEditingController();
+  
+
+ 
+  String infoResultado = "Classificação";
+ 
+  void _calcularSoma() {
+    setState(() {
+      int num = new Random().nextInt(11);
+      int aleatorio = int.parse(aleatorioController.text);
+      if(num == aleatorio)
+        infoResultado = 'Aleatório: $num\n\n Deu bom';
+      else
+        infoResultado = 'Aleatório: $num\n\n Mais sorte da próxima vez amigão';
+    });
+  }
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _titulo(),
+      backgroundColor: Colors.white,
+      body: _corpo(),
+    );
+  }
+ 
+  _titulo() {
+    return AppBar(
+      title: Text("Jogo do Nº Aleatório"),
+      centerTitle: true,
+      backgroundColor: Colors.red,
+    );
+  }
+ 
+  _corpo() {
+    return SingleChildScrollView(
+      padding: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Image.network('https://cdn-0.imagensemoldes.com.br/wp-content/uploads/2020/05/Imagem-Dado-PNG.png',
+          height: 150,),
+          _campo("Número de 0 a 10", aleatorioController),
+          _botao(),
+          _texto(infoResultado),
+        ],
+      ),
+    );
+  }
+ 
+  _campo(labelTitulo, objController) {
+    return TextField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+          labelText: labelTitulo, labelStyle: TextStyle(color: Colors.red)),
+      textAlign: TextAlign.center,
+      style: TextStyle(color: Colors.red, fontSize: 19.0),
+      controller: objController,
+    );
+  }
+ 
+  _botao() {
+    return Padding(
+      padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+      child: Container(
+        height: 50.0,
+        child: RaisedButton(
+          onPressed: _calcularSoma,
+          child: Text("Descobrir",
+              style: TextStyle(color: Colors.white, fontSize: 20.0)),
+          color: Colors.red,
+        ),
+      ),
+    );
+  }
+ 
+  _texto(textoParaExibir) {
+    return Text(
+      textoParaExibir,
+      textAlign: TextAlign.center,
+      style: TextStyle(color: Colors.red, fontSize: 25.0),
+    );
+  }
+}
