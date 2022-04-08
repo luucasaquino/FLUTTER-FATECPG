6e55df388d9fbbab61339ad053b9afa3eb7febb7
luucasaquino <aquiino.luucas@gmail.com>
2022-04-08 13:10:41 -0300

    App 4

---
+++ app04_gasolina/android/app/src/main/kotlin/com/example/app04_gasolina/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app04_gasolina
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app04_gasolina/lib/home.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController n1Controller = TextEditingController();
+  TextEditingController n2Controller = TextEditingController();
+
+  String infoResultado = "Informe os números!";
+
+  void _calcularSoma() {
+    setState(() {
+      double n1 = double.parse(n1Controller.text);
+      double n2 = double.parse(n2Controller.text);
+
+      double resultado = n1 / n2;
+      if (resultado <= 0.7)
+        infoResultado = 'O Etanol é melhor';
+      else
+        infoResultado = 'A Gasolina é melhor';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _titulo(),
+      backgroundColor: Colors.white,
+      body: _corpo(),
+    );
+  }
+
+  _titulo() {
+    return AppBar(
+      title: Text("Álcool ou Gasolina"),
+      centerTitle: true,
+      backgroundColor: Colors.green,
+    );
+  }
+
+  _corpo() {
+    return SingleChildScrollView(
+      padding: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Image.network(
+            'https://images.vexels.com/media/users/3/151307/isolated/preview/b4b009c6c791f8dced461e75c11cec60-ilustracao-do-tanque-de-gasolina.png',
+            height: 200,
+          ),
+          _campo("Digite o preço do Etanol", n1Controller),
+          _campo("Digite o preço da Gasolina", n2Controller),
+          _botao(),
+          _texto(infoResultado),
+        ],
+      ),
+    );
+  }
+
+  _campo(labelTitulo, objController) {
+    return TextField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+          labelText: labelTitulo, labelStyle: TextStyle(color: Colors.green)),
+      textAlign: TextAlign.center,
+      style: TextStyle(color: Colors.green, fontSize: 25.0),
+      controller: objController,
+    );
+  }
+
+  _botao() {
+    return Padding(
+      padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+      child: Container(
+        height: 50.0,
+        child: RaisedButton(
+          onPressed: _calcularSoma,
+          child: Text("Calcular",
+              style: TextStyle(color: Colors.white, fontSize: 20.0)),
+          color: Colors.blue,
+        ),
+      ),
+    );
+  }
+
+  _texto(textoParaExibir) {
+    return Text(
+      textoParaExibir,
+      textAlign: TextAlign.center,
+      style: TextStyle(color: Colors.red, fontSize: 25.0),
+    );
+  }
+}
